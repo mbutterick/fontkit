3c51f3b9286c894de785c8640cbb3daeab2b051b
Matthew Butterick <mbutterick@users.noreply.github.com>
2019-03-04 10:28:26 -0800

    Update CFFSubset.js

--- src/subset/CFFSubset.js
+++ src/subset/CFFSubset.js
@@ -156,7 +156,7 @@ export default class CFFSubset extends Subset {
     let top = {
       version: 1,
       hdrSize: this.cff.hdrSize,
-      offSize: this.cff.length,
+      offSize: 4, /* formerly `this.cff.length`, which prevented otf from working */
       header: this.cff.header,
       nameIndex: [this.cff.postscriptName],
       topDictIndex: [topDict],
